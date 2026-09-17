81710641dbdbec34c1713040c28ae96e0f2e59bb
Ansh Sharma <anshsharma161105@gmail.com>
2026-09-17 11:40:54 +0530

    fix cors for deployed frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({ origin: 'https://job-queue-orcin.vercel.app' });
 
   await app.listen(process.env.PORT ?? 3000);
 }
